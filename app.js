cd9694f426e1353302640ee4e046023703a925c0
sbiggs10 <thesadiebiggs@gmail.com>
2025-09-03 15:01:24 -0500

    app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+
+let flowers = 100; //this is a global variable
+
+flowers = flowers / 10;
+
+flowers = flowers + 65; //mathematical operations on variable 
+
+let message = "Hi! My name is Sadie Biggs and I have a dog named Daisy!"; //this is a string variable
+
+
+const person = {firstName:"Sadie", lastName:"Biggs", age:20, eyeColor:"blue"}; //object variable
+
+
+{
+    let blooms = 96;
+    
+    blooms = blooms + 36;
+}  // this is a block variable
+
+
+console.log(message);
+document.write("This is assignment 1!")
